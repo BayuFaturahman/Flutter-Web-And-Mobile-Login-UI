e5f12853ce45465ea12b0e1b9863e49ee55bae16
Ronaldo <ronaldinhomurakami@gmail.com>
2020-07-09 15:08:35 -0300

    Criação da tela para desktop

---
+++ lib/login/mobile_mode.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileMode extends StatefulWidget {
+  @override
+  _MobileModeState createState() => _MobileModeState();
+}
+
+class _MobileModeState extends State<MobileMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
